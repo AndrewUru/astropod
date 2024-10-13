4b36d0ddee31e49cddd9c0b4a04a30d3d299e76a
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2024-10-13 15:37:42 +0200

    confirmar

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
 import { z, defineCollection } from "astro:content";
 
 const episodeSchema = z.object({
-    title: z.string(),
-    audioUrl: z.string(),
-    pubDate: z.coerce.date().optional(),
-    cover: z.string().optional(),
-    explicit: z.boolean().optional(),
-    episode: z.number().optional(),
-    season: z.number().optional(),
-    episodeType: z.string().optional(),
-    duration: z.coerce.string(), //duration in format hh:mm:ss
-    size: z.number(), // size in megabytes
+  title: z.string(),
+  audioUrl: z.string(),
+  pubDate: z.coerce.date().optional(),
+  cover: z.string().optional(),
+  explicit: z.boolean().optional(),
+  episode: z.number().optional(),
+  season: z.number().optional(),
+  episodeType: z.string().optional(),
+  duration: z.coerce.string(), //duration in format hh:mm:ss
+  size: z.number().optional(), // size in megabytes
 });
 
 export type episodeSchema = z.infer<typeof episodeSchema>;
@@ -18,5 +18,5 @@ export type episodeSchema = z.infer<typeof episodeSchema>;
 const episodeCollection = defineCollection({ schema: episodeSchema });
 
 export const collections = {
-    'episode': episodeCollection,
-}
+  episode: episodeCollection,
+};
