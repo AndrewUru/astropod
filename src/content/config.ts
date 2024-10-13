a8f2bb2a463a5764cb7cdf0bc391ee3ab34a3243
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2024-10-13 16:38:24 +0200

    confirmar

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ const episodeSchema = z.object({
   audioUrl: z.string(),
   pubDate: z.coerce.date().optional(),
   cover: z.string().optional(),
+  description: z.string().optional(),
   explicit: z.boolean().optional(),
   episode: z.number().optional(),
   season: z.number().optional(),
