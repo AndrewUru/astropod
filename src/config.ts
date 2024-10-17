6fd1c091430af78098f564e77ed29d121eea92c6
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2024-10-17 19:03:36 +0200

    confirmar

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = "ElSaltoCast | Creamos tu PodCast GRATIS";
+export const SITE_TITLE = "ElSaltoCast | Creamos y mantenemos tu PodCast por 20€ mensuales";
 export const SITE_DESCRIPTION =
   "Este es un proyecto Open Source si quieres tener el tuyo contactanos.";
